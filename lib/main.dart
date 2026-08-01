4edf83d7515dd453e25a2ee6834563f2aa1d7d52
muhammedmufeed2 <mohammedmufeed738@gmail.com>
2026-08-01 11:51:57 +0530

    feat: real ui change

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _FirestoreExpenseScreenState extends State<FirestoreExpenseScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Firestore Expense Tracker'),
+        title: const Text('monusekuttan Expense Tracker'),
         backgroundColor: Colors.deepPurple,
       ),
       body: Padding(
