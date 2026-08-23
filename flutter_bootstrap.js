579b692beccb49851ae7bf5c6b59d5c26e5dfe9f
mufi10-tec <mufi10-tec@users.noreply.github.com>
2026-08-23 16:41:50 +0000

    deploy: 7b6f0491b03824c96908ffa4100e6ddc3995f73d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3923594832"
+    serviceWorkerVersion: "2822604168"
   }
 });
